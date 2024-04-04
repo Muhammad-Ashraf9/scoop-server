e1ff792ab462eb4215f6a3f234a3d248c02639cd
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-04-04 02:18:43 +0200

    Add helmet middleware for enhanced security

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
 require("dotenv").config();
 
 const authRouter = require("./routes/authenticationRoutes");
 
 const swaggerDocs = require("./utils/swagger");
 
-const server = express();
+const app = express();
+app.use(helmet());
 
 const port = process.env.PORT || 8080;
 
@@ -16,7 +18,7 @@ const port = process.env.PORT || 8080;
 mongoose
   .connect(`${process.env.DB_LINK}/${process.env.DB_NAME}`)
   .then(() => {
-    server.listen(port, () => {
+    app.listen(port, () => {
       console.log(`server is running on port: ${port}`);
     });
   })
@@ -25,29 +27,29 @@ mongoose
   });
 
 // serve swagger documentation
-swaggerDocs(server, port);
+swaggerDocs(app, port);
 
 // enable cors
-server.use(cors());
+app.use(cors());
 
-server.use(morgan("combined"));
+app.use(morgan("combined"));
 
 //////////////// parsing requests
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Auth Routes
-server.use("/api/v1/auth", authRouter);
+app.use("/api/v1/auth", authRouter);
 
-server.use(express.static("images"));
+app.use(express.static("images"));
 
 /////////////// Not found
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.status(404).json({ message: "Not Found" });
 });
 
 //////////////// Error handler
-server.use((error, req, res, next) => {
+app.use((error, req, res, next) => {
   console.log(error);
   res.status(error.statusCode || 500).json({ message: error + "" });
 });
